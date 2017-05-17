c5c52aa41bdb95171e6a47b154499297925e6b2a
AMDSIBYL <dingyupei24@gmail.com>
2017-05-17 16:56:56 +0800

    Add Arduino related scripts

---
+++ Assets/script/TutScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class TutScript : MonoBehaviour {
+
+	public float speed;
+	private float amountToMove;
+	
+	// check serial port (Arduino)
+	SerialPort sp = new SerialPort("/dev/cu.usbmodem1421", 115200);
+
+	// Use this for initialization
+	void Start () {
+		sp.Open ();
+		sp.ReadTimeout = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		amountToMove = speed * Time.deltaTime;
+
+		if (sp.IsOpen) {
+			try{
+				MoveObject(sp.ReadByte());
+				print(sp.ReadByte());
+			}
+			catch (System.Exception){
+			
+			}
+		}
+	}
+
+	void MoveObject(int Direction){
+		if (Direction == 1) {
+			//water box hahaha
+			//do something here...
+		}
+
+		if (Direction == 2) {
+			//mirror hehehe
+			//do something here...
+		}
+	}
+}
