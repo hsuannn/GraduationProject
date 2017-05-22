e83796794ee72437a0036f41c8713ea3a3a451b5
hsuannn <didi.ya35@gmail.com>
2017-05-22 09:08:23 +0800

    0521

---
+++ Assets/script/ChangeAlpha.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeAlpha : MonoBehaviour {
+	
+	Color startColor = new Color (152/256f, 114/256f, 114/256f, 0.01f);
+	Color endColor = new Color (152/256f, 114/256f, 114/256f, 0.25f);
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<SpriteRenderer> ().color = Color.Lerp(startColor, endColor, Mathf.PingPong (Time.time, 3));
+	}
+}
--- Assets/script/PlayVideo.cs
+++ Assets/script/PlayVideo.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayVideo : MonoBehaviour {
 
-	public GameObject Video,Footprint,Code;
+	public GameObject Flush;
+	private AudioSource flushAudio;
+
+	public GameObject Video, Footprint;
 	public MovieTexture movTexture; //電影材質
 	private AudioSource movAudio; //影片音軌
 
@@ -14,51 +17,67 @@ public class PlayVideo : MonoBehaviour {
 	public GameObject Scream;
 	private AudioSource screamAudio;
 
+	public GameObject WallCode, LastCode;
+
 	bool playedOnce = false;
 
 	// Use this for initialization
 	void Start () {
+		
 		Video.GetComponent<Renderer>().material.mainTexture = movTexture;
 		movTexture.loop = false;
+
+		flushAudio = Flush.GetComponent<AudioSource>();
 		movAudio = Video.GetComponent<AudioSource>();
 		beepAudio = Beep.GetComponent<AudioSource>();
 		screamAudio = Scream.GetComponent<AudioSource>();
+
 		Video.SetActive (false);
 		Footprint.SetActive (false);
-		Code.SetActive (false);
+		WallCode.SetActive (false);
+		LastCode.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		// Flush
 		if (Input.GetKeyDown(KeyCode.F1))
 		{
-			//GetComponent<AudioSource>().Play();
+			flushAudio.Play ();
+		}
+
+		// Mirrow
+		if (Input.GetKeyDown(KeyCode.F2))
+		{
 			Video.SetActive(true);
 			movTexture.Play();
 			movAudio.Play();
 			playedOnce = true;
 		}
-		if (Input.GetKeyDown(KeyCode.F2))
-		{
-			//GetComponent<AudioSource>().Pause();
-			//movTexture.Pause();
-			//movAudio.Pause();
-			beepAudio.Play ();
-		}
-		if (Input.GetKeyDown(KeyCode.F3))
-		{
-			//GetComponent<AudioSource>().Stop();
-			//movTexture.Stop();
-			//movAudio.Stop();
-			screamAudio.Play();
-		}
-		if (Input.GetKeyDown(KeyCode.F4))
-		{
-			Code.SetActive (true);
-		}
 		if (movTexture.isPlaying == false && playedOnce) {
 			Video.SetActive (false);
 			Footprint.SetActive (true);
 		}
+
+		// Beep (for footprint)
+		if (Input.GetKeyDown(KeyCode.F3))
+		{
+			beepAudio.Play ();
+		}
+
+		// Scream (for openning box)
+		if (Input.GetKeyDown(KeyCode.F4))
+		{
+			screamAudio.Play();
+		}
+
+		// Codes on the wall
+		if (Input.GetKeyDown(KeyCode.F5))
+		{
+			WallCode.SetActive (true);
+			LastCode.SetActive (true);
+		}
+
 	}
 }
